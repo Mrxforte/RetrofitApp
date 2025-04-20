0b1924f26751d54d58879f1fc0a14f0945893099
Azamat <urazimbetovazamat6@gmail.com>
2025-04-20 22:59:20 +0300

    reteofit debug

--- app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -7,21 +7,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofitapp.adapters.PostsAdapter
 import com.example.retrofitapp.api.ApiInterface
+import com.example.retrofitapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var postsAdapter: PostsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        postsAdapter = PostsAdapter()
 //        retrofit adding the project
         var retrofit = Retrofit
             .Builder()
@@ -30,8 +38,20 @@ class MainActivity : AppCompatActivity() {
             .build()
         var api = retrofit.create(ApiInterface::class.java)
         lifecycleScope.launch {
-            var response = api.getObjectsList()
-            Log.d("TAG", "onCreate: ${response.body()?.joinToString()}")
+            var response = api.getObjectsList().body()
+            response?.apply {
+                postsAdapter.submitList(this)
+            }
+            Log.d("TAG", "onCreate: ${response?.joinToString()}")
+        }
+        var myLayoutManager =
+            LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+
+        binding.rv.apply {
+            adapter = postsAdapter
+
+            layoutManager = myLayoutManager
+            addItemDecoration(DividerItemDecoration(this@MainActivity, myLayoutManager.orientation))
         }
     }
 }
--- app/src/main/java/com/example/retrofitapp/adapters/PostsAdapter.kt
+++ app/src/main/java/com/example/retrofitapp/adapters/PostsAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.retrofitapp.adapters
 
-class PostsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofitapp.databinding.PostsItemBinding
+import com.example.retrofitapp.models.PostsModel
+
+class PostsDiffUtil : DiffUtil.ItemCallback<PostsModel>() {
+    override fun areItemsTheSame(oldItem: PostsModel, newItem: PostsModel): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: PostsModel, newItem: PostsModel): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class PostsAdapter() : ListAdapter<PostsModel, PostsAdapter.MyViewHolder>(PostsDiffUtil()) {
+    inner class MyViewHolder(val binding: PostsItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: PostsModel) {
+            binding.name.text = post.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = PostsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
 }
